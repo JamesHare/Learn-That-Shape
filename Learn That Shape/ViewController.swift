273b341b243755945f8f4f8595cea120aad281a0
James Hare <james@notmyrealemail.com>
2018-12-01 23:09:23 -0600

    Initial Commit

---
+++ Learn That Shape/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn That Shape
+//
+//  Created by James Hare on 12/1/18.
+//  Copyright © 2018 James Hare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Learn That ShapeTests/Learn_That_ShapeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Learn_That_ShapeTests.swift
+//  Learn That ShapeTests
+//
+//  Created by James Hare on 12/1/18.
+//  Copyright © 2018 James Hare. All rights reserved.
+//
+
+import XCTest
+@testable import Learn_That_Shape
+
+class Learn_That_ShapeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
