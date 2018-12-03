7f08d2973128a6bdd68b6301914b5a0231c65273
James Hare <james@notmyrealemail.com>
2018-12-03 16:51:40 -0600

    SHAPE-10004 Added shape buttons to the UI view.

--- Learn That Shape/ViewController.swift
+++ Learn That Shape/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleBtn: UIButton!
+    @IBOutlet weak var squareBtn: UIButton!
+    @IBOutlet weak var triangleBtn: UIButton!
+    @IBOutlet weak var rectangleBtn: UIButton!
+    @IBOutlet weak var starBtn: UIButton!
+    @IBOutlet weak var heartBtn: UIButton!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let circleURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Circle", ofType: "wav")!)
+        let squareURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Square", ofType: "wav")!)
+        let triangleURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Triangle", ofType: "wav")!)
+        let rectangleURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Rectangle", ofType: "wav")!)
+        let starURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Star", ofType: "wav")!)
+        let heartURL = URL(fileURLWithPath: Bundle.main.path(forResource: "Heart", ofType: "wav")!)
     }
 
-
+    @IBAction func circleBtnPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func squareBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func triangleBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func rectangleBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func starBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func heartBtnPressed(_ sender: Any) {
+    }
 }
-
